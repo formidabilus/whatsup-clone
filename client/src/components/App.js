d9d66c038074461de107b3a4fa84e5a63c1c8c36
formidabilus <chiriac_razvan@ymail.com>
2021-03-12 20:30:10 +0200

    added OpenConversation component and more on ConversationProvider

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,21 @@
 import useLocalStorage from "../hooks/useLocalStorage";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
+import { ContactsProvider } from "../contexts/ContactsProvider";
+import { ConversationsProvider } from "../contexts/ConversationsProvider";
 
 function App() {
   const [id, setId] = useLocalStorage("id");
 
-  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
+  const dashboard = (
+    <ContactsProvider>
+      <ConversationsProvider id={id}>
+        <Dashboard id={id} />
+      </ConversationsProvider>
+    </ContactsProvider>
+  );
+
+  return id ? dashboard : <Login onIdSubmit={setId} />;
 }
 
 export default App;
